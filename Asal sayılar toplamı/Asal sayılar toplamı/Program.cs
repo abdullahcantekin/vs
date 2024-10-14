815e9fd26bff846cece050bf03e74aad0bc2975a
abdullahcantekin <157524159+abdullahcantekin@users.noreply.github.com>
2024-10-14 20:33:06 +0300

    Add files via upload

---
+++ Asal sayılar toplamı/Asal sayılar toplamı/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Asal_sayılar_toplamı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            Console.Write("Bir sayı girin (N): ");
+            int N = int.Parse(Console.ReadLine());
+
+            int sumOfPrimes = SumOfPrimes(N);
+
+            Console.WriteLine($"\n1'den {N}'e kadar olan asal sayıların toplamı: {sumOfPrimes}");
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        static int SumOfPrimes(int N)
+        {
+            int sum = 0;
+
+            for (int i = 2; i <= N; i++)
+            {
+                if (IsPrime(i))
+                {
+                    sum += i;
+                }
+
+            }
+
+            return sum;
+            #endregion
+        }
+
+
+        }
+}
+    
+
+
---
+++ Asal sayılar toplamı/Asal sayılar toplamı/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Asal sayılar toplamı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Asal sayılar toplamı")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f1a3a1ec-096e-48a7-a82c-033c549d5d7e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
